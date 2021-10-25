4392d7531c8a47505c8ba3258cbd925c6a94aeaf
Miriam Kifle <88691480+mkifle-dev@users.noreply.github.com>
2021-10-25 11:53:43 -0400

    Update database.js

--- Accountability-App/config/database.js
+++ Accountability-App/config/database.js
@@ -1,1 +1,10 @@
-// insert your data here
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://auth-1:auth@cluster0.tdpfk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+    'auth-1': 'auth'
+
+
+    
+};
+//const url = "mongodb+srv://user-one:BlueMoon@cluster0.5xllp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
